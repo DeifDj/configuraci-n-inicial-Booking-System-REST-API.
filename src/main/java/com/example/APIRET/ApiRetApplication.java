37a5595e90505537b0430fbf562810d370b2d923
DeifDj <clikball@gmail.com>
2023-11-11 01:06:00 -0500

    first commit

---
+++ src/main/java/com/example/APIRET/ApiRetApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIRET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIRET/ApiRetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIRET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
